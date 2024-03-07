df36f773614597417848bd80ee98ea9935105449
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-03-07 15:24:57 -0500

    Create Header.jsx

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <>
+      <h1 className="hero one">
+        MAYHEM<span>MILLS</span>
+      </h1>
+    </>
+  );
+}
+
+export default Header;
