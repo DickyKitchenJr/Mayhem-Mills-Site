8764a844d4373b497dae8845af86ec12b29664f9
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-03-07 15:25:04 -0500

    Create Home.jsx

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,27 @@
+import "./Home.css";
+import Header from "../Components/Header";
+import Author from "../Components/Author";
+import BookOne from "../Components/BookOne";
+import BookTwo from "../Components/BookTwo";
+import BookThree from "../Components/BookThree";
+import BookFour from "../Components/BookFour";
+import ContactMe from "../Components/ContactMe";
+import Footer from "../Components/Footer";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Author />
+      <h2 className="spacer">BOOKS</h2>
+      <BookOne />
+      <BookTwo />
+      <BookThree />
+      <BookFour />
+      <ContactMe />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
