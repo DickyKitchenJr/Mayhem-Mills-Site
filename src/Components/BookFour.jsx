0af126884d7721f01998c9c81d51c7f92fa6c67f
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-03-07 15:24:19 -0500

    Create BookFour.jsx

---
+++ src/Components/BookFour.jsx
@@ -0,0 +1,46 @@
+import { Parallax } from "react-parallax";
+import Book4 from "../Images/IslemanBook.webp";
+import Isleman from "../Images/Isleman.webp";
+import "./Books.css";
+
+const BookFour = () => (
+  <Parallax
+    contentClassName="books"
+    blur={1}
+    bgImage={Isleman}
+    bgImageAlt="mountain backdrop"
+    strength={500}
+    bgClassName="mountains"
+  >
+    <div className="book-content">
+      <p className="bookp">
+        "Fresh and gripping...a Scotland you haven't experienced before..."
+      </p>
+      <img className="bookcover" src={Book4} alt="Isleman book cover" />
+    </div>
+    <h3 className="booktitle">Isleman: A Novel of Scotland</h3>
+    <p className="bookblurb">
+      The King of Scots is dead and restless clans have risen, their thirst for
+      power and vengeance threatening to ignite an inferno that will consume the
+      Kingdom of Scotland. Thomas Skene, banished to the wilds of Scotland, grew
+      up without family nor home. Now his only friend has been summoned back to
+      the Isle of Skye with Thomas in tow, but the secret he carries will not
+      stay a secret for long.
+      <br />
+      <br />
+      Pursued by enemies, hunted by assassins, and caught up in the cunning game
+      of an empty throne, Nial and Thomas are forced to navigate the bloody
+      waters of clan politics before the kingdom itself explodes into warfare
+      and rebellion.
+      <br />
+      <br />
+      Get Isleman and find yourself transported to the perilous and intoxicating
+      Scottish Highlands of the sixteenth century.
+      <br />
+      <br />
+      Coming Soon!
+    </p>
+  </Parallax>
+);
+
+export default BookFour;
